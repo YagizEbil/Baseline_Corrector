d30b2d71645ffa7c3e76a7a7c55937baf66b0dd1
YagizEbil <kyagizebil@gmail.com>
2023-02-06 15:24:31 +0300

    data

--- main.py
+++ main.py
@@ -15,12 +15,12 @@ y_corrected = arPLS_baseline_correction(y)
 baseline_fitter = Baseline(x_data=x)
 bkg_1 = baseline_fitter.mor(y, half_window=30)[0]
 
-plt.plot(x, y, label='Original')
+#plt.plot(x, y, label='Original')
 #plt.plot(x, y_corrected, label='arPLS')
 #plt.plot(x, y-y_corrected, label="arPLS_Corrected")
-plt.plot(x,bkg_1,label="mor")
-plt.plot(x,y-bkg_1,label="mor_Corrected")
-plt.plot(x, moving_average(y-bkg_1, window_size=10) , label ="smother")
+#plt.plot(x,bkg_1,label="mor")
+#plt.plot(x,y-bkg_1,label="mor_Corrected")
+plt.plot(x, moving_average(y-bkg_1, window_size=10) , label ="Corrected")
 
 plt.title(os.path.basename(xyreader.fileHandle.name))
 plt.xlabel("X Axis")
